4a379eaf8b04c733fb53d1183d1774b11049026e
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2023-01-10 15:45:28 +0100

    set for firebase hosting setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD8_P4F3PbA6mGuED2FxH3wpT-DkzImTyI",
+    authDomain: "r0901534-project-mob-apps.firebaseapp.com",
+    projectId: "r0901534-project-mob-apps",
+    storageBucket: "r0901534-project-mob-apps.appspot.com",
+    messagingSenderId: "283044105757",
+    appId: "1:283044105757:web:e54e4748af03d4d09680f4"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD8_P4F3PbA6mGuED2FxH3wpT-DkzImTyI",
+    authDomain: "r0901534-project-mob-apps.firebaseapp.com",
+    projectId: "r0901534-project-mob-apps",
+    storageBucket: "r0901534-project-mob-apps.appspot.com",
+    messagingSenderId: "283044105757",
+    appId: "1:283044105757:web:e54e4748af03d4d09680f4"
+  }
 };
 
 /*
